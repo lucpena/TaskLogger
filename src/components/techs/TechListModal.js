3770f1daafc6d77e87747e8a68ba39930f229386
Lucas Araújo Pena <lucaspena4@gmail.com>
2020-06-26 14:57:33 -0300

    Add files via upload

---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import TechItem from './TechItem';
+import { getTechs } from '../../actions/techActions'
+
+export const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
+    useEffect(() => {
+        getTechs();
+        // eslint-diable-next-line
+    }, []);
+
+    return (
+        <div id="tech-list-modal" className="modal">
+            <div className="modal-content">
+                <h4>Technician list</h4>
+
+                <ul className="collection">
+                    {!loading && techs !== null &&
+                    techs.map(tech => (
+                            <TechItem tech={tech} key={tech.id} />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+TechListModal.propTypes = {
+    tech: PropTypes.object.isRequired,
+    getTechs: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    tech: state.tech
+});
+
+export default connect(mapStateToProps, { getTechs })(TechListModal);
